97fe202d532cc2d9d8f2573d5e71f2eb75758b13
HsYooon <sssu0000@gmail.com>
2022-06-06 20:08:08 +0900

    :hammer: 이름 변경, 메소드 이동

--- src/main/java/com/domelist/dome/controller/MainController.java
+++ src/main/java/com/domelist/dome/controller/MainController.java
@@ -1,257 +1,124 @@
 package com.domelist.dome.controller;
 
-import com.domelist.dome.dto.CdDto;
+import com.domelist.dome.dto.DeliveryDto;
+import com.domelist.dome.dto.DomeDto;
 import com.domelist.dome.dto.MarketPostDto;
+import com.domelist.dome.dto.SiteInfoDto;
+import com.domelist.dome.service.CommonService;
 import com.domelist.dome.service.DomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     DomeService service;
 
-    /* 운영 경로 */
-    private String path = "/var/lib/tomcat9/webapps/upload/";
-    /* 로컬 경로 */
-    //private String path = "/Users/hapsun/Desktop/study/img/";
+    @Autowired
+    CommonService commonService;
 
-    @RequestMapping(value = "/ads.txt")
-    @ResponseBody
-    public String adstxt(HttpServletResponse response) {
-        String fileName = "ads.txt";
-        response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
-        String content = "google.com, pub-1708982640263259, DIRECT, f08c47fec0942fa0";
-        return content;
+    /* 메인 */
+    @GetMapping("/")
+    public String home(Model model) {
+        /* 12개의 상품 */
+        List<DomeDto> mainNewList = service.mainNewList();
+        /* 12개의 상품 */
+        List<DomeDto> mainBestList = service.mainBestList();
+        model.addAttribute("todayProductList", mainNewList);
+        model.addAttribute("bestProductList", mainBestList);
+        /* 최신 마케팅 소식 12개 */
+        List<MarketPostDto> mainMarketPostList = service.marketInfoPostList2("01",12);
+        /* 인기 사이트 top10 */
+        //todo: dto 정리
+        List<Object> siteInfoListByCnt = service.siteInfoListByCnt();
+        model.addAttribute("marketPostList", mainMarketPostList);
+        model.addAttribute("siteInfoListByCnt", siteInfoListByCnt);
+        return "home";
     }
 
-    @PostMapping("/search")
-    public String searchPrd(String query, Model model) {
-        System.out.print("query : " + query);
-        Map<String,Object> result =  service.searchPrd(query);
-        model.addAttribute("query", query);
-        model.addAttribute("searchList", result.get("list"));
-        model.addAttribute("cnt", result.get("cnt"));
-        return "prdDefault";
-    }
-
-    /* 필독 정보 모음 */
-    @GetMapping("/marketInfo")
-    public String marketInfo(Model model) {
-        List<MarketPostDto> result1 = service.marketInfoPostList2("01",8);
-        List<MarketPostDto> result2 = service.marketInfoPostList2("02",8);
-        List<MarketPostDto> result3 = service.marketInfoPostList2("03",8);
-        List<MarketPostDto> result4 = service.marketInfoPostList2("04",8);
-        model.addAttribute("infoPostList", result1);
-        model.addAttribute("productPostList", result2);
-        model.addAttribute("academyPostList", result3);
-        model.addAttribute("programPostList", result4);
-        return "marketInfo";
-    }
-
-    /* 필독 정보 모음 > 최신 마케팅 소식 */
-    @GetMapping("/marketInfo/info")
-    public String marketInfo01(Model model) {
-        List<MarketPostDto> result1 = service.marketInfoPostList("01");
-        List<MarketPostDto> result2 = service.marketInfoPostListByCnt("01");
-        model.addAttribute("marketInfoPostList", result1);
-        model.addAttribute("popularPostList", result2);
-        return "marketInfo_01";
-    }
-
-    /* 필독 정보 모음 > 마케팅 달력 */
-    @GetMapping("/marketInfo/calendar")
-    public String marketInfo02() {
-        return "marketInfo_02";
-    }
-
-    /* 필독 정보 모음 > 마케팅 상품 모음 */
-    @GetMapping("/marketInfo/product")
-    public String marketInfo03(Model model) {
-        List<MarketPostDto> result = service.marketInfoPostList("02");
-        model.addAttribute("marketInfoPostList", result);
-        return "marketInfo_03";
-    }
-
-    /* 필독 정보 모음 > 10억 아카데미 */
-    @GetMapping("/marketInfo/academy")
-    public String marketInfo04(Model model) {
-        List<MarketPostDto> result1 = service.marketInfoPostList("03");
-        List<MarketPostDto> result2 = service.marketInfoPostListByCnt("03");
-        model.addAttribute("marketInfoPostList", result1);
-        model.addAttribute("popularPostList", result2);
-        return "marketInfo_04";
-    }
-
-    /* 필독 정보 모음 > 필수프로그램 모음 */
-    @GetMapping("/marketInfo/program")
-    public String marketInfo05(Model model) {
-        List<MarketPostDto> result = service.marketInfoPostList("04");
-        model.addAttribute("marketInfoPostList", result);
-        return "marketInfo_05";
-    }
-
-    /* 필독 정보 모음 > 사업자를 위한 정보 모음 */
-    @GetMapping("/marketInfo/business")
-    public String marketInfo06(Model model) {
-        List<MarketPostDto> result = service.marketInfoPostList("05");
-        model.addAttribute("marketInfoPostList", result);
-        return "marketInfo_06";
-    }
-
-    /* 필독 정보 모음 > 게시글 */
-    @GetMapping("/marketInfo/post")
-    public String infoMarket(@RequestParam("id") int id, Model model, HttpSession session) {
-        // 게시글 조회
-        MarketPostDto result1 = service.marketPost(id);
-        // 전체 인기게시글 3개 조회
-        int cnt = 3;
-        List<MarketPostDto> result2 = service.allInfoPostListByCnt(cnt);
-        String role = (String)session.getAttribute("role");
-
-        if(role == "admin"){
-            model.addAttribute("role", role);
-        }else {
-            // 관리자 아닌경우 조회수 증가
-            service.updatePostCount(id);
+    /* 사이트 정보 */
+    @GetMapping("/siteinfo")
+    public String siteInfo(@RequestParam("cd") String cd, Model model) {
+        /* cd = null or all : 모든 사이트*/
+        List<SiteInfoDto> siteInfoList = service.siteInfoList(cd);
+        List<Object> siteInfoListByCnt = service.siteInfoListByCnt();
+        model.addAttribute("siteInfoListByCnt", siteInfoListByCnt);
+        String title = "";
+        if(cd.equals("all")) {
+            model.addAttribute("title", title);
+            model.addAttribute("siteInfoList", siteInfoList);
+            return "siteinfo";
         }
-
-        model.addAttribute("post", result1);
-        model.addAttribute("popularPostList", result2);
-        model.addAttribute("id", id);
-        return "marketInfo_post";
+        title = service.categoryNm(cd);
+        model.addAttribute("title", title);
+        model.addAttribute("siteInfoList", siteInfoList);
+        return "siteinfo";
     }
 
-    /* 로그인 */
-    @GetMapping("/login")
-    public String login_form() {
-        return "login";
+    @GetMapping("/delivery")
+    public String delivery(Model model) {
+        Map<String, List> result = service.deliveryMainLists();
+        model.addAttribute("domesticList", result.get("domestic"));
+        model.addAttribute("abroadList", result.get("abroad"));
+        model.addAttribute("advertiseList", result.get("advertise"));
+        return "delivery";
     }
 
-    /* 로그인 확인 */
-    @PostMapping("/login_confirm")
-    public String login(@RequestParam String id, @RequestParam String password, HttpSession session) {
-        if (id.equals("admin") && password.equals("domelist")) {
-            session.setAttribute("role", "admin");
-            return "redirect:/writePost";
-        }else {
-            return "redirect:/";
-        }
+    @GetMapping("/delivery/request")
+    public String deliveryRequest() {
+        return "delivery_request";
     }
 
-    /* 게시글 작성폼 */
-    @GetMapping("/writePost")
-    public String marketInfoWritePost(Model model) {
-        List<CdDto> result = service.infoCategoryList();
-        model.addAttribute("infoCategoryList",result);
-        return "writePost";
+    @GetMapping("/delivery/domestic")
+    public String deliveryDomesticInfo(Model model){
+        String code = "domestic";
+        String title = "3PL 물류/택배 대행 업체 정보 모음";
+        String desc ="3PL 물류/택배 대행 업체 정보를 한자리에 모았습니다. 비지니스 성장을 위한 인사이트를 얻을 수 있는지 살펴 보세요.";
+        List<DeliveryDto> result = service.deliveryDomesticList();
+        model.addAttribute("code", code);
+        model.addAttribute("title",title);
+        model.addAttribute("desc", desc);
+        model.addAttribute("deliveryList", result);
+        return "delivery_info";
     }
 
-    /* 게시글 수정 */
-    @GetMapping("/modifyPost")
-    public String marketInfoModifyPost(@RequestParam("id") String id, Model model) {
-
-        int origin_id = Integer.parseInt(id);
-        MarketPostDto result1 = service.marketPost(origin_id);
-        List<CdDto> result2 = service.infoCategoryList();
-
-        model.addAttribute("infoCategoryList",result2);
-        model.addAttribute("post", result1);
-        model.addAttribute("id",id);
-        return "modifyPost";
+    @GetMapping("/delivery/abroad")
+    public String deliveryAbroadInfo(Model model){
+        String code = "abroad";
+        String title = "해외 수입구매 (대량구매/OEM/ODM) 대행 업체 정보 모음";
+        String desc ="해외 수입구매 대행 업체 정보를 한자리에 모았습니다. 비지니스 성장을 위한 인사이트를 얻을 수 있는지 살펴 보세요.";
+        List<DeliveryDto> result = service.deliveryAbroadList();
+        model.addAttribute("code", code);
+        model.addAttribute("title",title);
+        model.addAttribute("desc", desc);
+        model.addAttribute("deliveryList", result);
+        return "delivery_info";
     }
 
-    /* 게시글 > 식제 */
-    @GetMapping("/deletePost")
-    public String marketInfoDeletePost(@RequestParam("id") String id) {
-        int origin_id = Integer.parseInt(id);
-        System.out.println("id ===> " + origin_id);
-        service.deletePost(origin_id);
-        return "redirect:/marketInfo";
+    @GetMapping("/advertise")
+    public String advertiseInfo(Model model) {
+        String code = "advertise";
+        Map<String, List> result = service.advertiseLists();
+        model.addAttribute("code", code);
+        model.addAttribute("searchList", result.get("searchList"));
+        model.addAttribute("shoppingList", result.get("shoppingList"));
+        model.addAttribute("daList", result.get("daList"));
+        model.addAttribute("reviewList", result.get("reviewList"));
+        return "advertise";
     }
 
-    /* 게시글 작성 > 업로드*/
-    @PostMapping("/marketInfo/upload")
-    public String marketInfoUpload(MultipartHttpServletRequest request) throws IOException {
-        Map<String,String> post = new HashMap<>();
-        post.put("title", request.getParameter("title"));
-        post.put("writer", request.getParameter("writer"));
-        post.put("article", request.getParameter("article"));
-        post.put("category", request.getParameter("category"));
-
-        /* 썸네일 없는 경우 패스 */
-        MultipartFile thumbnail = request.getFile("thumbnail");
-        System.out.println("upload post ==? " + thumbnail);
-        if(!thumbnail.isEmpty()) {
-            String name = UUID.randomUUID() + thumbnail.getOriginalFilename();
-            File imageFile = new File(path + name);
-            try {
-                thumbnail.transferTo(imageFile);
-                post.put("thumbnail", name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        service.uploadPost(post);
-        return "redirect:/marketInfo";
+    @GetMapping("/marketing")
+    public String marketingInfo(){
+        return "marketing";
     }
 
-    /* 게시글 수정 */
-    @PostMapping("/marketInfo/modify")
-    public String marketInfoModifyPost(MultipartHttpServletRequest request) throws IOException {
-
-        Map<String,Object> post = new HashMap<>();
-
-        post.put("title", request.getParameter("title"));
-        post.put("writer", request.getParameter("writer"));
-        post.put("article", request.getParameter("article"));
-        post.put("category", request.getParameter("category"));
-
-        MultipartFile thumbnail = request.getFile("thumbnail");
-        /* 썸네일 없는 경우 패스 */
-        if(!thumbnail.isEmpty()) {
-            String name = UUID.randomUUID() + thumbnail.getOriginalFilename();
-            File imageFile = new File(path + name);
-            try {
-                thumbnail.transferTo(imageFile);
-                post.put("thumbnail", name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        post.put("id", request.getParameter("id"));
-        System.out.println("modify params ===> " + post.toString());
-        service.modifyPost(post);
-        return "redirect:/marketInfo";
-    }
-
-    /* 이미지 다운로드 -> imageController 로 이동해야함 */
-    @GetMapping(value = "/image/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
-    @ResponseBody
-    public byte[] userSearch(@PathVariable("filename") String filename) throws IOException {
-        File file = new File(path+filename);
-        FileInputStream inputStream = new FileInputStream(file);
-        byte[] bytes = new byte[inputStream.available()];
-        inputStream.read(bytes, 0, inputStream.available());
-        return bytes;
-    }
 }
